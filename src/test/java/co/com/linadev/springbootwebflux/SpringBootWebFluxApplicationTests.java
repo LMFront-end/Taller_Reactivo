e6d22620568bcf829dd9d1bb5e514a68f2243558
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-17 19:47:25 -0500

    se crea conexion a la DB

---
+++ src/test/java/co/com/linadev/springbootwebflux/SpringBootWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.linadev.springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
